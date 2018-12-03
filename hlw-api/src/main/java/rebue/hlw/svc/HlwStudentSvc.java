6be31c2ea0edec38d44e5d9eba9eaa2144faf3c5
zbz <nnzbz@qq.com>
2018-12-03 10:02:53 +0800

    + 初始化

---
+++ hlw-api/src/main/java/rebue/hlw/svc/HlwStudentSvc.java
@@ -0,0 +1,13 @@
+package rebue.hlw.svc;
+
+import rebue.robotech.svc.MybatisBaseSvc;
+import rebue.hlw.mo.HlwStudentMo;
+
+/**
+ * 学生信息
+ *
+ * @mbg.generated 自动生成的注释，如需修改本注释，请删除本行
+ */
+public interface HlwStudentSvc extends MybatisBaseSvc<HlwStudentMo, java.lang.Long>{
+
+}
---
+++ hlw-svr/src/main/java/rebue/hlw/ctrl/HlwStudentCtrl.java
@@ -0,0 +1,191 @@
+package rebue.hlw.ctrl;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.pagehelper.PageInfo;
+
+import rebue.hlw.mo.HlwStudentMo;
+import rebue.hlw.svc.HlwStudentSvc;
+import rebue.robotech.dic.ResultDic;
+import rebue.robotech.ro.Ro;
+
+/**
+ * 学生信息
+ *
+ * @mbg.generated 自动生成的注释，如需修改本注释，请删除本行
+ */
+@RestController
+public class HlwStudentCtrl {
+    /**
+     * @mbg.generated 自动生成，如需修改，请删除本行
+     */
+    private final static Logger _log             = LoggerFactory.getLogger(HlwStudentCtrl.class);
+
+    /**
+     * @mbg.generated 自动生成，如需修改，请删除本行
+     */
+    @Resource
+    private HlwStudentSvc       svc;
+
+    /**
+     * 有唯一约束的字段名称
+     *
+     * @mbg.generated 自动生成，如需修改，请删除本行
+     */
+    private final String        _uniqueFilesName = "某字段内容";
+
+    /**
+     * 添加学生信息
+     *
+     * @mbg.generated 自动生成，如需修改，请删除本行
+     */
+    @PostMapping("/hlw/student")
+    Ro add(@RequestBody final HlwStudentMo mo) throws Exception {
+        _log.info("add HlwStudentMo: {}", mo);
+        final Ro ro = new Ro();
+        try {
+            final int result = svc.add(mo);
+            if (result == 1) {
+                final String msg = "添加成功";
+                _log.info("{}: mo-{}", msg, mo);
+                ro.setMsg(msg);
+                ro.setResult(ResultDic.SUCCESS);
+                return ro;
+            } else {
+                final String msg = "添加失败";
+                _log.error("{}: mo-{}", msg, mo);
+                ro.setMsg(msg);
+                ro.setResult(ResultDic.FAIL);
+                return ro;
+            }
+        } catch (final DuplicateKeyException e) {
+            final String msg = "添加失败，" + _uniqueFilesName + "已存在，不允许出现重复";
+            _log.error("{}: mo-{}", msg, mo);
+            ro.setMsg(msg);
+            ro.setResult(ResultDic.FAIL);
+            return ro;
+        } catch (final RuntimeException e) {
+            final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            final String msg = "添加失败，出现运行时异常(" + sdf.format(new Date()) + ")";
+            _log.error(msg + ": mo=" + mo, e);
+            ro.setMsg(msg);
+            ro.setResult(ResultDic.FAIL);
+            return ro;
+        }
+    }
+
+    /**
+     * 修改学生信息
+     *
+     * @mbg.generated 自动生成，如需修改，请删除本行
+     */
+    @PutMapping("/hlw/student")
+    Ro modify(@RequestBody final HlwStudentMo mo) throws Exception {
+        _log.info("modify HlwStudentMo: {}", mo);
+        final Ro ro = new Ro();
+        try {
+            if (svc.modify(mo) == 1) {
+                final String msg = "修改成功";
+                _log.info("{}: mo-{}", msg, mo);
+                ro.setMsg(msg);
+                ro.setResult(ResultDic.SUCCESS);
+                return ro;
+            } else {
+                final String msg = "修改失败";
+                _log.error("{}: mo-{}", msg, mo);
+                ro.setMsg(msg);
+                ro.setResult(ResultDic.FAIL);
+                return ro;
+            }
+        } catch (final DuplicateKeyException e) {
+            final String msg = "修改失败，" + _uniqueFilesName + "已存在，不允许出现重复";
+            _log.error(msg + ": mo=" + mo, e);
+            ro.setMsg(msg);
+            ro.setResult(ResultDic.FAIL);
+            return ro;
+        } catch (final RuntimeException e) {
+            final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            final String msg = "修改失败，出现运行时异常(" + sdf.format(new Date()) + ")";
+            _log.error("{}: mo-{}", msg, mo);
+            ro.setMsg(msg);
+            ro.setResult(ResultDic.FAIL);
+            return ro;
+        }
+    }
+
+    /**
+     * 删除学生信息
+     * 
+     * @mbg.generated 自动生成，如需修改，请删除本行
+     */
+    @DeleteMapping("/hlw/student")
+    Ro del(@RequestParam("id") final java.lang.Long id) {
+        _log.info("del HlwStudentMo by id: {}", id);
+        final int result = svc.del(id);
+        final Ro ro = new Ro();
+        if (result == 1) {
+            final String msg = "删除成功";
+            _log.info("{}: id-{}", msg, id);
+            ro.setMsg(msg);
+            ro.setResult(ResultDic.SUCCESS);
+            return ro;
+        } else {
+            final String msg = "删除失败，找不到该记录";
+            _log.error("{}: id-{}", msg, id);
+            ro.setMsg(msg);
+            ro.setResult(ResultDic.FAIL);
+            return ro;
+        }
+    }
+
+    /**
+     * 查询学生信息
+     * 
+     * @mbg.generated 自动生成，如需修改，请删除本行
+     */
+    @GetMapping("/hlw/student")
+    PageInfo<HlwStudentMo> list(final HlwStudentMo mo, @RequestParam(value = "pageNum", required = false) Integer pageNum,
+            @RequestParam(value = "pageSize", required = false) Integer pageSize) {
+        if (pageNum == null) {
+            pageNum = 1;
+        }
+        if (pageSize == null) {
+            pageSize = 5;
+        }
+        _log.info("list HlwStudentMo:" + mo + ", pageNum = " + pageNum + ", pageSize = " + pageSize);
+        if (pageSize > 50) {
+            final String msg = "pageSize不能大于50";
+            _log.error(msg);
+            throw new IllegalArgumentException(msg);
+        }
+        final PageInfo<HlwStudentMo> result = svc.list(mo, pageNum, pageSize);
+        _log.info("result: " + result);
+        return result;
+    }
+
+    /**
+     * 获取单个学生信息
+     * 
+     * @mbg.generated 自动生成，如需修改，请删除本行
+     */
+    @GetMapping("/hlw/student/getbyid")
+    HlwStudentMo getById(@RequestParam("id") final java.lang.Long id) {
+        _log.info("get HlwStudentMo by id: " + id);
+        return svc.getById(id);
+    }
+
+}
