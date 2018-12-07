a6b37ff8a8576a6e173715201846fc8f94d837c6
zbz <nnzbz@qq.com>
2018-12-07 16:53:33 +0800

    + 单元测试

--- hlw-svr/src/main/java/rebue/hlw/HlwApplication.java
+++ hlw-svr/src/main/java/rebue/hlw/HlwApplication.java
@@ -1,17 +1,14 @@
-package rebue.hlw.ctrl;
+package rebue.hlw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//这个注解是为了使该包内的过滤器生效。
+// 这个注解是为了使该包内的过滤器生效。
 @ServletComponentScan("rebue.sbs.smx.filter")
 @SpringCloudApplication
-@EnableFeignClients(basePackages = { "rebue.suc.svr.feign" })
 public class HlwApplication {
-
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(HlwApplication.class, args);
     }
 
