b97c5018e92433c2e646636797a51c426a683f08
zbz <nnzbz@qq.com>
2018-12-22 13:58:56 +0800

    * 重新生成代码

--- hlw-api/src/main/java/rebue/hlw/svc/HlwStudentSvc.java
+++ hlw-api/src/main/java/rebue/hlw/svc/HlwStudentSvc.java
@@ -4,14 +4,14 @@ import java.util.List;
 import rebue.hlw.dic.SexDic;
 import rebue.hlw.jo.HlwStudentJo;
 import rebue.hlw.mo.HlwStudentMo;
-import rebue.robotech.svc.MybatisBaseSvc;
+import rebue.robotech.svc.BaseSvc;
 
 /**
  * 学生信息
  *
  * @mbg.generated 自动生成的注释，如需修改本注释，请删除本行
  */
-public interface HlwStudentSvc extends MybatisBaseSvc<HlwStudentMo, java.lang.Long> {
+public interface HlwStudentSvc extends BaseSvc<java.lang.Long, HlwStudentMo, HlwStudentJo> {
 
     /**
      * 通过性别获取学生信息列表
