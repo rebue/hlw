63e29d5fbafbae7f56a31dc1871fd4ee912d27bc
zbz <nnzbz@21cn.com>
2019-12-01 19:53:58 +0800

    + 加入slueth

--- hlw-svr/src/main/java/com/github/rebue/hlw/HlwApplication.java
+++ hlw-svr/src/main/java/com/github/rebue/hlw/HlwApplication.java
@@ -3,11 +3,23 @@ package com.github.rebue.hlw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
 
 // 这个注解是为了使该包内的过滤器生效。
 @ServletComponentScan("rebue.sbs.smx.filter")
 @SpringCloudApplication
+@Slf4j
+@RestController
 public class HlwApplication {
+    @RequestMapping("/")
+    public String home() {
+        log.info("Handling home");
+        return "Hello World";
+    }
+
     public static void main(final String[] args) {
         SpringApplication.run(HlwApplication.class, args);
     }
