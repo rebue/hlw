0baf9627afd29a7918b13f0e9dbe26cbdbcc7ed8
zbz <nnzbz@21cn.com>
2019-12-01 19:54:16 +0800

    * 包名改变

--- hlw-bll/src/main/java/com/github/rebue/hlw/dao/HlwStudentDao.java
+++
@@ -1,7 +0,0 @@
-package com.github.rebue.hlw.dao;
-
-import com.github.rebue.hlw.jo.HlwStudentJo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HlwStudentDao extends JpaRepository<HlwStudentJo, java.lang.Long> {
-}
