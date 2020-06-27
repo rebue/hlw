febe678444236ec06e2c12e303075177f69f5267
zbz <nnzbz@21cn.com>
2020-06-27 18:40:33 +0800

    * 设置无符号类型

--- db/script/create-table.sql
+++ db/script/create-table.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2020/6/22 19:54:23                           */
+/* Created on:     2020/6/27 18:33:44                           */
 /*==============================================================*/
 
 
@@ -11,17 +11,17 @@ drop table if exists HLW_STUDENT;
 /*==============================================================*/
 create table HLW_STUDENT
 (
-   ID                   bigint not null  comment '学生ID',
+   ID                   bigint unsigned not null  comment '学生ID',
    STUDENT_CODE         varchar(20) not null  comment '学号',
    NAME                 varchar(100) not null  comment '姓名',
    PHONE1               varchar(20) not null  comment '电话号码1',
    PHONE2               varchar(20) not null  comment '电话号码2',
    ID_CARD              char(18) not null  comment '身份证号码',
-   AGE                  tinyint  comment '年龄',
+   AGE                  tinyint unsigned  comment '年龄',
    BIRTHDAY             date  comment '生日',
-   SEX                  tinyint  comment '性别',
-   HEIGHT               numeric(3,2)  comment '身高',
-   WEIGHT               numeric(5,2)  comment '体重',
+   SEX                  tinyint unsigned  comment '性别',
+   HEIGHT               numeric(3,2) unsigned  comment '身高',
+   WEIGHT               numeric(5,2) unsigned  comment '体重',
    IS_CPC               bool  comment '是否党员',
    REG_DATETIME         datetime  comment '注册时间',
    COMMUNICATE_TIME     time  comment '每天沟通时间',
